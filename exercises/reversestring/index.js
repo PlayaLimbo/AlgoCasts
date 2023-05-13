23e722c1a1a18b2a426d728217b3d6c0ca47a341
PlayaLimbo <xplayalimbox92@hotmail.com>
2023-05-13 01:01:14 -0500

    problem one

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,10 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split("").reduce((group, el) => {
+        return el + group;
+    }, "");
+}
 
 module.exports = reverse;
