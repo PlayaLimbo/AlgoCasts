26fbbfeef960587c80a7a19e03b346b874f4ac05
PlayaLimbo <xplayalimbox92@hotmail.com>
2023-05-23 09:58:45 -0500

    Update index.js

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,46 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    const matrix = [];
+
+    for (let i = 0; i < n; i++) {
+        matrix.push([]);
+    }
+    let counter = 1;
+    let startCol = 0;
+    let startRow = 0;
+    let endCol = n - 1;
+    let endRow = n - 1;
+    // while (startCol <= endCol && startRow <= endRow) {
+    //     // top row
+    //     for (let i = startCol; i <= endCol; i++) {
+    //         matrix[startRow][i] = counter;
+    //         counter++;
+    //     }
+    //     startRow++;
+    //     // right column
+    //     for (let i = startRow; i <= endRow; i++) {
+    //         matrix[i][endCol] = counter;
+    //         counter++;
+    //     }
+    //     endCol--;
+    //     // bottom row
+    //     for (let i = endCol; i >= startCol; i--) {
+    //         matrix[endRow][i] = counter;
+    //         counter++;
+    //     }
+    //     endRow--;
+    //     // start col
+    //     for (let i = endRow; i >= startRow; i--) {
+    //         matrix[i][startCol] = counter;
+    //         counter++;
+    //     }
+    //     startCol++;
+    // }
+
+    console.log(matrix);
+    return matrix;
+}
 
 module.exports = matrix;
